3e59a0e7517741b9ae7dac1ea4e8efcb003ad017
adeblieux <113622999+adeblieux@users.noreply.github.com>
2024-09-04 13:37:10 -0500

    kirb

---
+++ sketch.js
@@ -0,0 +1,53 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  function draw() {
+    background("white");
+////
+fill(217,30,75);
+strokeWeight(4);
+stroke(164,53,97);
+ellipse(250, 380, 50,90);
+ellipse(350, 380, 50,90);
+////
+fill(255,190,230);
+strokeWeight(4);
+stroke(164,53,97);
+ellipse(390, 250, 80,80);
+ellipse(width/2, height/2, 180,180);
+////
+strokeWeight(2);
+stroke(0,3,1);
+fill(10,12,22);
+ellipse(261, 260, 25,50);
+ellipse(333, 260, 25,50);
+////
+fill(9,45,177);
+strokeWeight(2);
+stroke(1,28,144);
+ellipse(width/2.3, height/2.2, 10,15);
+ellipse(width/1.8, height/2.2, 10,15);
+////
+fill("black");
+strokeWeight(1);
+noStroke();
+ellipse(261, 258, 20,25);
+ellipse(333, 258, 20,25);
+////
+fill(255,255,255);
+strokeWeight(2);
+stroke(53,88,182);
+ellipse(261, 248, 20,25);
+ellipse(333, 248, 20,25);
+////
+fill(255,190,230);
+strokeWeight(4);
+stroke(164,53,97);
+ellipse(210, 210, 90,100);
+////
+fill(255,104,171);
+noStroke();
+ellipse(240, 300, 25,15);
+ellipse(350, 300, 25,15);
+////
+  }
